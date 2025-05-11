fa86c09c371dd2cbffb9723d101ccfc33e27ff23
Venkata Pavan <venkatapavan2905@gmail.com>
2025-05-11 10:42:25 +0530

    updated side note

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -2,6 +2,6 @@ resource "aws_vpc" "nautilus-vpc" {
     cidr_block = "10.0.0.0/16"
     assign_generated_ipv6_cidr_block = true   //aws prvided ipv6 cidr block
     tags = {
-        Name = "nautilus-vpc"
+        Name = "nautilus-vpc"          //tags is mandatory to name vpc using terraform
     }
 }
