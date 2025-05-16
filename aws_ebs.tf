121a995e80891ba1507ee0e765b18e3ba1915716
Venkata Pavan <venkatapavan2905@gmail.com>
2025-05-16 11:07:26 +0530

    updated file with ebs volume snapshot resource

--- aws_ebs.tf
+++ aws_ebs.tf
@@ -1,9 +1,19 @@
+#ebs volume
 resource "aws_ebs_volume" "devops-volume" {
   type = "gp3"
   size = 2
   availability_zone = "us-east-1a"
   tags = {
     Name = "devops-volume"
+  }
+}
+
+# ebs volume snapshot
+resource "aws_ebs_snapshot" "devops-vol-ss" {
+  description = "Devops Snapshot"
+  volume_id   = aws_ebs_volume.devops-volume.id
+  tags = {
+    Name = "devops-vol-ss"
   }
 }
 
