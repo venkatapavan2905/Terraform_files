1cdd2ba140fe423423098ff08ac68d166edde3f2
Venkata Pavan <venkatapavan2905@gmail.com>
2025-05-18 16:15:48 +0530

    Added policy

--- aws_s3.tf
+++ aws_s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "xfusion-s3-26640" {
     bucket = "xfusion-s3-26640"
+    ##acl = "public-read"
     tags = {
         Name = "xfusion-s3-26640"
     }
@@ -17,4 +18,21 @@ resource "aws_s3_bucket_public_access_block" "xfusion_public_access" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "xfusion_bucket_policy" {
+  bucket = aws_s3_bucket.xfusion-s3-26640.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.xfusion-s3-26640.arn}/*"
+      }
+    ]
+  })
 }
