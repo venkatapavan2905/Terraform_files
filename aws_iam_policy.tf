f33ff652748c5df00bde1a47cd7bc1733360f465
Venkata Pavan <venkatapavan2905@gmail.com>
2025-05-23 16:05:39 +0530

    aws iam policy

---
+++ aws_iam_policy.tf
@@ -0,0 +1,15 @@
+resource "aws_iam_policy" "policy" {
+  name = "iampolicy_javed"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
