ba3ef986cf94efe776e9377b0025fe23799efc87
Venkata Pavan <venkatapavan2905@gmail.com>
2025-06-03 09:11:39 +0530

    updated user

--- aws_iam_policy_attachment.tf
+++ aws_iam_policy_attachment.tf
@@ -1,15 +1,15 @@
 # Create IAM user
 resource "aws_iam_user" "user" {
-  name = "iamuser_ravi"
+  name = "iamuser_pavan"
 
   tags = {
-    Name = "iamuser_ravi"
+    Name = "iamuser_pavan"
   }
 }
 
 # Create IAM Policy
 resource "aws_iam_policy" "policy" {
-  name        = "iampolicy_ravi"
+  name        = "iampolicy_pavan"
   description = "IAM policy allowing EC2 read actions for ravi"
 
   policy = jsonencode({
