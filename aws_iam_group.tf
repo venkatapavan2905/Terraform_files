0bd5786b2852a743f1fe1ba8179b519db363b793
Venkata Pavan <venkatapavan2905@gmail.com>
2025-06-07 19:54:50 +0530

    aws iam commands

--- aws_iam_group.tf
+++ aws_iam_group.tf
@@ -1,3 +1,9 @@
 resource "aws_iam_group" "group" {
   name = "iamgroup_javed"
 }
+
+#Commands:
+#1. aws iam list=groups
+#2. aws iam get-group --group-name <group_name>
+#3. aws iam create-group --group-name <group_name>
+#4. aws iam delete-group --group-name <group_name>
