5bd43a8fab897279a58d38c547dc5029970103d4
Venkata Pavan <venkatapavan2905@gmail.com>
2025-06-12 10:15:46 +0530

    updated variables.tf

--- variables.tf
+++ variables.tf
@@ -3,9 +3,19 @@ variable "KKE_vpc" {
   default = "datacenter-vpc"
 }
 
+variable "KKE_sg" {
+  type    = string
+  default = "devops-sg"
+}
+
+resource "aws_security_group" "sg" {
+  name = var.KKE_sg
+}
+
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = var.KKE_vpc
   }
 }
+
