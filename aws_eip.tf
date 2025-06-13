5b2a014f356de06b38e0516df48adc7eade39673
Venkata Pavan <venkatapavan2905@gmail.com>
2025-06-13 10:46:24 +0530

    updated aws_eip with variable

--- aws_eip.tf
+++ aws_eip.tf
@@ -1,6 +1,11 @@
+variable "KKE_eip" {
+  type    = string
+  default = "nautilus-eip"
+}
+
 resource "aws_eip" "nautilus-eip" {
     domain = "vpc"
     tags = {
-        Name = "nautilus-eip"
+        Name = var.KKE_eip
     }
 }
