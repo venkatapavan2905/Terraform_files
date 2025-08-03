53ed8c23ce815f9e1ffaa7cd8662b3f6b364a6ab
Venkata Pavan <venkatapavan2905@gmail.com>
2025-08-03 21:49:00 +0530

    aws_datastream

--- aws_datastream.tf
+++ aws_datastream.tf
@@ -6,3 +6,34 @@ resource "aws_kinesis_stream" "stream" {
   }
 
 }
+
+### Kinesis with cloudwatch alarm
+
+resource "aws_kinesis_stream" "kinesis" {
+  name             = "datacenter-kinesis-stream"
+  shard_count      = 1
+  retention_period = 24
+  shard_level_metrics = [
+    "IncomingBytes",
+    "IncomingRecords",
+    "OutgoingBytes",
+    "OutgoingRecords",
+    "WriteProvisionedThroughputExceeded",
+    "ReadProvisionedThroughputExceeded",
+    "IteratorAgeMilliseconds"
+  ]
+}
+
+resource "aws_cloudwatch_metric_alarm" "alarm" {
+  alarm_name          = "datacenter-kinesis-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "WriteProvisionedThroughputExceeded"
+  namespace           = "AWS/kinesis"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 1
+  dimensions = {
+    StreamName = aws_kinesis_stream.kinesis.name
+  }
+}
