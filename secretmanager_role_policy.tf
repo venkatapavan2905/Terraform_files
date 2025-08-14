9066362f73cae6bb1264f01ce6ddb04c62ddabe9
Venkata Pavan <venkatapavan2905@gmail.com>
2025-08-14 21:25:57 +0530

    secretmanager_role_policy

---
+++ secretmanager_role_policy.tf
@@ -0,0 +1,60 @@
+# Add your code below
+resource "aws_secretsmanager_secret" "secret" {
+    name = var.KKE_SECRET_NAME
+}
+
+resource "aws_secretsmanager_secret_version" "version" {
+    secret_id = aws_secretsmanager_secret.secret.id
+    secret_string = var.KKE_SECRET_VALUE
+}
+
+resource "aws_iam_role" "role" {
+    name = var.KKE_ROLE_NAME
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Effect = "Allow"
+            Principal = {
+                Service = "ec2.amazonaws.com"
+            }
+            Action = "sts:AssumeRole"
+        }]
+    })
+}
+
+resource "aws_iam_policy" "policy" {
+    name = var.KKE_POLICY_NAME
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Effect = "Allow"
+            Action = ["secretsmanager:GetSecretValue"]
+            Resource = aws_secretsmanager_secret.secret.arn
+        }]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+    role = aws_iam_role.role.name
+    policy_arn = aws_iam_policy.policy.arn
+}
+
+variable "KKE_SECRET_NAME" {
+    type = string
+    default = "datacenter-app-secret"
+}
+
+variable "KKE_SECRET_VALUE" {
+    type = string
+    default = "{\"db_user\":\"admin\",\"db_pass\":\"supersecret\"}"
+}
+
+variable "KKE_ROLE_NAME" {
+    type = string
+    default = "datacenter-app-role"
+}
+
+variable "KKE_POLICY_NAME" {
+    type = string
+    default = "datacenter-app-policy"
+}
