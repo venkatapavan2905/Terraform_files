6266444bdd1a2d6094c985e0a9deb9e5e3cc5886
Venkata Pavan <venkatapavan2905@gmail.com>
2025-08-15 11:23:56 +0530

    secretmanager_role_policy

--- secretmanager_role_policy.tf
+++ secretmanager_role_policy.tf
@@ -1,43 +1,45 @@
 # Add your code below
+# Add your code below
 resource "aws_secretsmanager_secret" "secret" {
-    name = var.KKE_SECRET_NAME
+  name = var.KKE_SECRET_NAME
 }
 
-resource "aws_secretsmanager_secret_version" "version" {
-    secret_id = aws_secretsmanager_secret.secret.id
-    secret_string = var.KKE_SECRET_VALUE
+resource "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id     = aws_secretsmanager_secret.secret.id
+  secret_string = var.KKE_SECRET_VALUE
 }
 
 resource "aws_iam_role" "role" {
-    name = var.KKE_ROLE_NAME
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [{
-            Effect = "Allow"
-            Principal = {
-                Service = "ec2.amazonaws.com"
-            }
-            Action = "sts:AssumeRole"
-        }]
-    })
+  name = var.KKE_ROLE_NAME
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
-resource "aws_iam_policy" "policy" {
-    name = var.KKE_POLICY_NAME
-    policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [{
-            Effect = "Allow"
-            Action = ["secretsmanager:GetSecretValue"]
-            Resource = aws_secretsmanager_secret.secret.arn
-        }]
-    })
+resource "aws_iam_role_policy" "inline_policy" {
+  name = var.KKE_POLICY_NAME
+  role = aws_iam_role.role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "secretsmanager:GetSecretValue"
+      ],
+      Resource = aws_secretsmanager_secret.secret.arn
+    }]
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "attach" {
-    role = aws_iam_role.role.name
-    policy_arn = aws_iam_policy.policy.arn
-}
 
 variable "KKE_SECRET_NAME" {
     type = string
@@ -58,3 +60,5 @@ variable "KKE_POLICY_NAME" {
     type = string
     default = "datacenter-app-policy"
 }
+
+#There is a difference between attaching inline polcies and managed polices.
