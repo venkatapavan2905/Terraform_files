871b7f4f6f2c29a32a7cd52f81cbaf820f0fcb56
Venkata Pavan <venkatapavan2905@gmail.com>
2025-08-18 22:04:11 +0530

    Dynamodb

---
+++ Level-3/dynambodb.tf
@@ -0,0 +1,33 @@
+resource "aws_dynamodb_table" "table" {
+    name = "var.KKE_TABLE_NAME"
+    billing_mode = "PAY_PER_REQUEST"
+    #primary key also called as Hash Key
+    hash_key = "taskId"
+    attribute {
+        name = "taskId"
+        type = "S"
+    }
+    tags = {
+        Name = "var.KKE_TABLE_NAME"
+    }
+}
+
+resource "aws_dynamodb_table_item" "item1" {
+    table_name = aws_dynamodb_table.table.name
+    hash_key = "taskId"
+    item = jsonencode({
+        taskId = {"S" = "1" }
+        description = {"S" = "Learn DynamoDB"}
+        status = {"S" = "completed" }
+    })
+}
+
+resource "aws_dynamodb_table_item" "item2" {
+    table_name = aws_dynamodb_table.table.name
+    hash_key = "taskId"
+    item = jsonencode({
+        taskId = {"S" = "2"}
+        description = {"S" = "Build To-Do App"}
+        status = {"S" = "in-progress"}
+    })
+}
